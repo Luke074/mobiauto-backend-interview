61e117fd28f5f6ac1d22117abb9f586580cc1963
Lucas Alves <68557804+Luke074@users.noreply.github.com>
2024-02-04 00:04:36 -0300

    fix: ajustando sql

--- src/main/resources/db/migration/oportunidades.sql
+++ src/main/resources/db/migration/oportunidades.sql
@@ -1,7 +1,8 @@
 CREATE TABLE oportunidades (
     id INTEGER AUTOINCREMENT PRIMARY KEY,
     cliente_id INTEGER NOT NULL,
-    status_oportunidade ENUM('novo', 'em atendimento', 'concluido') DEFAULT 'novo' NOT NULL,
+    status_oportunidade ENUM('NOVO', 'EM_ATENDIMENTO', 'CONCLUIDO') DEFAULT 'NOVO' NOT NULL,
     data_aplicacao DATE NOT NULL,
     data_conclusao DATE NOT NULL,
+    revenda_id INTEGER,
 )
--- src/main/resources/db/migration/usuarios.sql
+++ src/main/resources/db/migration/usuarios.sql
@@ -3,6 +3,6 @@ CREATE TABLE usuarios (
     nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     senha VARCHAR(255) NOT NULL,
-    cargo ENUM('proprietario', 'gerente', 'assistente') DEFAULT 'assistente',
+    cargo ENUM('PROPRIETARIO', 'GERENTE', 'ASSISTENTE') DEFAULT 'ASSISTENTE',
     loja_id INTEGER, 
 )
